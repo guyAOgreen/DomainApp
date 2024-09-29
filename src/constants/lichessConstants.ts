c1d2e4b1292a885db65ba099ee5d7c67a27b5f12
gugreen <guy.green@oracle.com>
2024-09-29 19:14:36 +0200

    Adding a chess tab

---
+++ src/constants/lichessConstants.ts
@@ -0,0 +1,4 @@
+
+export const numberOfRecentGamesShown = 4;
+
+export const embeddedLichessUrl = (id: String) =>`https://lichess.org/embed/${id}?theme=auto&bg=auto`;
---
+++ src/pages/ChessPage/ChessPage.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import axios from "axios";
+import { extractLichessGameIds } from "../../utils/lichessUtils";
+import RecentGamesContent from "../../components/Tab/TabContent/RecentGamesContent";
+import { numberOfRecentGamesShown } from "../../constants/lichessConstants";
+import { ChessPageTabs } from "../../components/Tab/tabConstants";
+import TabButton from "../../components/Tab/TabButton";
+
+import './ChessPage.css';
+import LinksContent from "../../components/Tab/TabContent/LinksContent";
+
+const ChessPage = () => {
+    const [games, setGames] = React.useState<String>("");
+    const [gamesIds, setGamesIds] = React.useState<String []>([]);
+    const [loaded, setLoaded] = React.useState<Boolean>(false);
+
+    const [displayTab, setDisplayTab] = React.useState<number>(ChessPageTabs.RecentGames);
+
+    React.useEffect(() => {
+        const fetchGames = async () => {
+          try {
+            const response = await axios.get(
+                `https://lichess.org/api/games/user/guygreenInClassAtUCT?max=${numberOfRecentGamesShown}`
+            );      
+            setGames(response.data);
+          } catch (error) {
+                console.error("Error fetching games:", error);
+          }
+        };
+    
+        fetchGames();
+      }, []);
+   
+    React.useEffect(() => {
+        if (games?.length > 0) {
+            setGamesIds(extractLichessGameIds(games));
+        }
+    }, [setGamesIds, games]);
+
+
+    if (gamesIds.length === numberOfRecentGamesShown && !loaded) {
+        setLoaded(true);
+    }
+
+    const renderTabs = () => {
+        switch(displayTab) {
+            case ChessPageTabs.RecentGames:
+                if (loaded) {
+                    return  (
+                        <RecentGamesContent
+                            gamesIds={gamesIds}
+                        />
+                    );
+                } else {
+                    return (
+                        <div> 
+                            <p>Loading Page</p>
+                        </div>
+                    );
+                }
+
+            case ChessPageTabs.links:
+                return (
+                    <LinksContent/>
+                );
+            default:
+                return null;
+        }
+    }
+
+   
+    return (
+        <>
+            <div className="title">
+                {"My Chess Life"}
+            </div>
+            <>
+                <div className="tab-container">
+                    <TabButton
+                        title={"Recent Games"}
+                        showContentFn={setDisplayTab}
+                        page={ChessPageTabs.RecentGames}
+                        isActive={displayTab === ChessPageTabs.RecentGames}
+                    />
+                    <TabButton
+                        title={"Find Me"}
+                        showContentFn={setDisplayTab}
+                        page={ChessPageTabs.links}
+                        isActive={displayTab === ChessPageTabs.links}
+                    />
+                </div>
+            </>
+
+            {renderTabs()}
+        </>
+      );
+}
+export default ChessPage;
